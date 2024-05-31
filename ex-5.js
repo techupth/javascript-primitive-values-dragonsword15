248806367efa3f467ea6134dc4d9bfb4a30a8fd5
pongpetr <suerdragon8995@gmail.com>
2024-05-31 22:47:52 +0700

    bp3-pang

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discount = NaN * 25 / 100;
+let priceToPay = totalPrice * 25 / 100;
+console.log(priceToPay)
+ 
